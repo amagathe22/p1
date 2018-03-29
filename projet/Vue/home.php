80cb36f4df91a42be0945e63fbc7e748ac3d0353
Philippe <philippe@MacBookAir-5855CAF628CD-2.local>
2018-03-29 14:56:07 +0200

    Salut

--- projet/Vue/home.php
+++ projet/Vue/home.php
@@ -3,12 +3,18 @@
 <head>
 	<title>Bienvenue</title>
 </head>
-<body>
-	<h1>Bienvenue sur l'accueil</h1>
-	<form action="index.php?page=login" method="post">
-		<input type="text" name="login">
-		<input type="password" name="password">
-		<input type="submit" name="submit">
-	</form>
+<body style="background-image:url(http://www.technopole-anticipa.com/IMG/arton950.jpg)">
+	<h1 style="text-align: center; color:blue">BIENVENUE SUR LINKEDON, LE SITE NUMÉRO 1 DES SITES DE RECRUTEMENT EN LIGNE!</h1>
+	<h2 style="text-align: center; color:darkblue">CONNECTEZ VOUS:</h2>
+	<div style="text-align: center;">
+		<form action="index.php?page=login" method="post">
+			LogIn:<br>
+			<input type="text" name="login">
+			Mot de Passe:<br>
+			<input type="password" name="password">
+			<input type="submit" name="submit">
+		</form>
+		<p>Vous ne possédez pas encore de compte? inscrivez vous au plus vite!</p>
+	</div>
 </body>
 </html>

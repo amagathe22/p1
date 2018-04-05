4fd65d146a1cca1ec7b2f7d773e8a48203ba9ad2
Philippe <philippe@MacBookAir-5855CAF628CD-2.local>
2018-04-05 17:50:51 +0200

    j

---
+++ projet/Modèle/user.php
@@ -0,0 +1,36 @@
+<?php
+/**
+* 
+*/
+class User 
+{
+	private $name;
+	private $mail;
+	private $id;
+	private $password;
+	function __construct($name,$mail,$pass)
+	{
+		$this->name=$name;
+		$this->mail=$mail;
+		$this->password=$pass;
+	}
+	function getName(){
+		return $this->name;
+	}
+	function getMail(){
+		return $this->mail;
+	}
+	function setName($name){
+		$this->name=$name;
+	}
+	function setMail($mail){
+		$this->mail=$mail;
+	}
+	function getPassword(){
+		return $this->password;
+	}
+	function setPassword($pass){
+		$this->password=$pass;
+	}
+}
+?>

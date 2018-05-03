380b8b3ddebb8b50ca0173e47530974731906e4c
Philippe <philippe@MacBookAir-5855CAF628CD-2.local>
2018-05-03 09:14:13 +0200

    created

--- projet/Vue/created.php
+++ projet/Vue/created.php
@@ -5,10 +5,10 @@
 </head>
 <body>
 	<br><br><br><br><br><br><br>
-<h1>Vous etes inscrits sur LinkedON !</h1>
+<h1>Vous êtes inscrit sur LINKEDON !</h1>
 <br><br><br><br>
 <form action="index.php?page=login" method="post">
-    <input type="submit" value="deconnexion">
+    <input type="submit" value="Retour au login">
 </form>
 </body>
 </html>
